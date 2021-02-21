53b56c605957506ac006f7bdc2cfc37169c16945
CharlieCaramels <mio780308@gmail.com>
2021-02-21 22:45:33 +0800

    blah

--- src/Blah.js
+++ src/Blah.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import ListItemData from "./structs"
 
 export default class Blah extends React.Component{
     render(){
-        return <h3>BLAH</h3>;
+        let data=[];
+
+        data.push(new ListItemData("blah1","../img/chinesePavilion.png","3D modeling practice during class",false));
+        data.push(new ListItemData("blah2","../img/chinesePavilion.png","3D modeling practice during class",false));
+        data.push(new ListItemData("blah3","../img/chinesePavilion.png","3D modeling practice during class",false));
+
+        return (
+        <div className="">
+            {data.map(function(d,idx){
+                return <h2>{d.name}</h2>;
+                })}
+        </div>);
     }
 }
---
+++ src/structs.js
@@ -0,0 +1,10 @@
+class ListItemData{
+    constructor(name,mediaLink,description,hasVideo){
+        this.name=name;
+        this.mediaLink=mediaLink;
+        this.description=description;
+        this.hasVideo=hasVideo;
+    }
+}
+
+export default ListItemData;
